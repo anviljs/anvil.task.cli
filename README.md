763852a159df5e9721d7c6dbb1e5205f21b50425
Alex Robson <asrobson@gmail.com>
2012-12-06 22:09:21 -0600

    Initial checkin

---
+++ README.md
@@ -0,0 +1,7 @@
+## Anvil Task Command Line Interface
+
+This extension is a core component of anvil and is required in order to interact with tasks from the command line.
+
+## Installation
+
+anvil will install this plugin during post-install.
---
+++ src/task.js
@@ -0,0 +1,71 @@
+var taskFactory = function( _, anvil ) {
+    anvil.command( {
+        name: "anvil.task",
+        commander: {
+            "tasks": {
+                action: "list",
+                description: "list out tasks"
+            },
+            "*": {
+                action: "run",
+                description: "run task"
+            }
+        },
+
+        getTask: function( taskName ) {
+             return anvil.extensions.tasks[ taskName ];
+        },
+
+        getList: function( taskName, list, missing ) {
+            var self = this;
+            if( list[ taskName ] ) {
+                return;
+            } else {
+                var task = this.getTask( taskName );
+                if( task ) {
+                    if( task.dependencies.length ) {
+                        _.each( task.dependencies, function( dependency ) {
+                            self.getList( dependency, list, missing );
+                        } );
+                    }
+                    list[ taskName ] = task;
+                } else {
+                    missing.push( taskName );
+                }
+            }
+        },
+
+        run: function( taskName, options, done ) {
+            var tasks = {},
+                missing = [];
+            this.getList( taskName, tasks, missing );
+            if( missing.length ) {
+                anvil.log.error( "The following tasks could not be found: " );
+                    _.each( missing, function( dependency ) {
+                        anvil.log.error( "   " + dependency );
+                    } );
+            } else {
+                var sorted = anvil.utility.dependencySort( tasks, "ascending", function( a, b ) {
+                    return a.name === b.name;
+                } );
+                var calls = _.pluck( tasks, "run" );
+                anvil.scheduler.pipeline( undefined, calls, done );
+            }
+        },
+
+        
+        list: function( options, done ) {
+            console.log( "Installed tasks:" );
+            var limit = 30,
+                padding = new Array( limit ).join( " " );
+            _.each( anvil.extensions.tasks, function( task ) {
+                var name = ( task.name + padding ).substr( 0, limit ),
+                    description = task.description || "No description provided";
+                console.log( "  " + name + description );
+            } );
+            done();
+        }
+    } );
+};
+
+module.exports = taskFactory;
